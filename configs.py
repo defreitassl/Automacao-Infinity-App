a359bf3465431a0d425ac56b73fb55a11f5577dd
Douglas Freitas <douglasamf14@gmail.com>
2024-09-20 17:17:33 -0300

    .

--- configs.py
+++ configs.py
@@ -1,9 +1,8 @@
-import os
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-driver_path = os.path.join(os.getcwd(), 'chromedriver-linux64', 'chromedriver')
 
-service = Service(executable_path=driver_path)
+service = Service(ChromeDriverManager().install())
 
 driver = webdriver.Chrome(service=service)
