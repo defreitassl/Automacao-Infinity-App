195f71e1c42ff2e7085e5393291e99eb93dafe2d
Douglas Freitas <douglasamf14@gmail.com>
2024-09-20 17:19:24 -0300

    fixing bugs

--- configs.py
+++ configs.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-
 service = Service(ChromeDriverManager().install())
 
 driver = webdriver.Chrome(service=service)
